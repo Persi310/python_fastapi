b445e872956807640f094e6f0f94f33d63f9cccf
Persi Josue Casado Ochoa <persijosue.mariokart@gmail.com>
2026-06-08 10:46:54 -0600

    añadiendo seguridad a credenciales

--- Curso Python/backend/FastAPI/db/client.py
+++ Curso Python/backend/FastAPI/db/client.py
@@ -1,10 +1,8 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+from config import Settings
 
-#OPjLTG7PvB4lYXUR
-uri = "mongodb+srv://admin:OPjLTG7PvB4lYXUR@cluster0.36fx70y.mongodb.net/?appName=Cluster0"
-
-db_client = MongoClient(uri, server_api=ServerApi('1')).Cluster0
+db_client = MongoClient(Settings.URI, server_api=ServerApi('1')).Cluster0
 try:
     db_client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
